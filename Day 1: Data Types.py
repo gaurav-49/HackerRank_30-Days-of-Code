bffee308f3b775917376e1fe2bbbea0f75302958
Gaurav Sharma <57179984+gaurav-49@users.noreply.github.com>
2020-07-20 15:39:20 +0530

    Create Day 1: Data Types.py

---
+++ Day 1: Data Types.py
@@ -0,0 +1,10 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+i2 = int(input()) # read int</pre>
+d2 = float(input()) # read double
+s2 = input() # read string
+# print summed and concatenated values
+print(i + i2)
+print(d + d2)
+print(s + s2)
